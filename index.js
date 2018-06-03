9b7eaa4fdbf512ba923ebda4fd156dc3dd2f3b37
Mihyaeru <mihyaeru21@gmail.com>
2018-06-04 01:44:22 +0900

    :pencil2: add JSDoc

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var castv2 = require('castv2-client');
 var tts = require('google-tts-api');
 
+/**
+ * @param {string} ip Google Home's ip address
+ * @param {string} [language] default: 'en'
+ * @param {number} [speed] default: 1
+ */
 var GoogleHomePlayer = function(ip, language, speed) {
   var self = this;
 
@@ -8,6 +13,12 @@ var GoogleHomePlayer = function(ip, language, speed) {
   this.language = language || 'en';
   this.speed = speed || 1;
 
+  /**
+   * @param {string} text a text to play
+   * @param {string} [language] default: this.language
+   * @param {number} [speed] default: this.speed
+   * @return {Promise<void>}
+   */
   this.say = function(text, language, speed) {
     language = language || self.language;
     speed = speed || self.speed;
@@ -16,6 +27,11 @@ var GoogleHomePlayer = function(ip, language, speed) {
     });
   };
 
+  /**
+   * @param {string} url a sound url to play
+   * @param {string} [contentType] default: 'audio/mp3'
+   * @return {Promise<void>}
+   */
   this.play = function(url, contentType) {
     return new Promise(function(resolve, reject) {
       var client = new castv2.Client();
